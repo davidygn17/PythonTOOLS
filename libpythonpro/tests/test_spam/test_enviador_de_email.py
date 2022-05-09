7df31b7a37b338854e8ad65b9c6badec1356f0fc
David Nunes <98975085+davidygn17@users.noreply.github.com>
2022-05-09 22:49:50 +0100

    Estrutura basica Pytest

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,11 +1,13 @@
 import pytest
 
-from libpythonpro.tests.enviador_de_email import Enviador
+from libpythonpro.tests.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
+
+
 @pytest.mark.parametrize(
     'destinatario',
     ['davidygn17@gmail.com', 'davidyurigarcianunes93@gmail.com']
@@ -21,3 +23,18 @@ def test_remetente(destinatario):
         'fazendo teste pra aprender como tudo isso funciona'
     )
     assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['','david']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'margaridafernandes05@gmail.com',
+            'teste de curso DEVPRO',
+            'fazendo teste pra aprender como tudo isso funciona'
+        )
---
+++ libpythonpro/tests/test_spam/teste_usuarios.py
@@ -0,0 +1,24 @@
+from libpythonpro.spam.db import Conexao
+from libpythonpro.spam.modelos import Usuario
+
+
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuario = Usuario(nome='David')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
+
+def test_listar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='David'), Usuario(nome='Margarida')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
